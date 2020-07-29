34394661c7949321bb0946dda0c923efa91ab989
nieguozhang <ngz0324@163.com>
2020-07-29 20:26:46 +0800

    更新

---
+++ 03.数据解析/07.xpath解析基础.py
@@ -0,0 +1,1 @@
+from lxml import etreedef main():    # 若出现html代码书写不规范，不符合xml解析器的使用规范，使用parse方法的parser参数    parser = etree.HTMLParser(encoding="utf-8")    tree = etree.parse('sougou.html', parser=parser)    r = tree.xpath('//div[@class="top-nav"]//@href')    print(r)if __name__ == '__main__':    main()
