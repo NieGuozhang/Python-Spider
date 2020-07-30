441c4daec25ff45c6f03f84c5aca2d32d773e441
NieGuozhang <ngz0324@163.com>
2020-07-30 13:09:13 +0800

    Update 05.bs4解析基础.py

--- 03.数据解析/05.bs4解析基础.py
+++ 03.数据解析/05.bs4解析基础.py
@@ -1,1 +1,22 @@
-from bs4 import BeautifulSoupdef main():    # 将本地的html文档加载到该对象中    # 参数2固定    fp = open('./sougou.html', 'r', encoding='utf-8')    soup = BeautifulSoup(fp, 'lxml')    # print(soup.a) # soup.tagName返回的是HTML中第一次出现的tagName标签    # print(soup.find('a')['href'])  # 作用和soup.a一样    # 返回class为top-nav的div    print(soup.find('div', class_='top-nav').text)    # print(soup.findAll('a'))    # print(soup.select('.top-nav'))    # print(soup.select('.top-nav > ul a').get_text())if __name__ == '__main__':    main()
+from bs4 import BeautifulSoup
+
+
+def main():
+    # 将本地的html文档加载到该对象中
+    # 参数2固定
+    fp = open('./sougou.html', 'r', encoding='utf-8')
+    soup = BeautifulSoup(fp, 'lxml')
+    # print(soup.a) # soup.tagName返回的是HTML中第一次出现的tagName标签
+    # print(soup.find('a')['href'])  # 作用和soup.a一样
+    # 返回class为top-nav的div
+    print(soup.find('div', class_='top-nav').text)
+    # print(soup.findAll('a'))
+    # print(soup.select('.top-nav'))
+
+    # print(soup.select('.top-nav > ul a').get_text())
+
+
+if __name__ == '__main__':
+    main()
+
+    
